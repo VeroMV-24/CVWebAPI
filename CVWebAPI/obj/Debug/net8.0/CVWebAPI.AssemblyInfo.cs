ee53ec5ae4be433c4cbb19d48c8d0a2d0ecfc656
Veronica <veronica.mendoza.vidal@gmail.com>
2025-06-03 13:58:55 -0600

    Inicial

--- CVWebAPI/obj/Debug/net8.0/CVWebAPI.AssemblyInfo.cs
+++ CVWebAPI/obj/Debug/net8.0/CVWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CVWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ffcbee8126787522a5501de5a340686c16b4cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CVWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CVWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
