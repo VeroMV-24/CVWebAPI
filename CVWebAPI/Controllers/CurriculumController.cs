a4ffcbee8126787522a5501de5a340686c16b4cc
Veronica <veronica.mendoza.vidal@gmail.com>
2025-06-03 13:54:25 -0600

    Inicial

---
+++ CVWebAPI/Controllers/CurriculumController.cs
@@ -0,0 +1,81 @@
+﻿using CVWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CVWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CurriculumController : Controller
+    {
+        [HttpGet]
+        public ActionResult<Curriculum> GetCurriculum()
+        {
+            var curriculum = new Curriculum
+            {
+                Name = "Verónica de Jesús Mendoza Vidal",
+                Birthdate = new DateTime(1996, 02, 03),
+                SkillsData = new List<SkillGroup>
+                {
+                  new SkillGroup
+                  {
+                      Title = "Lenguajes de Programación y Tecnologias",
+                      Skills = new List<Skill>
+                      {
+                          new Skill {Name= "HTML", StartDate= "Septiembre 2020", EndDate= "Presente"},
+                          new Skill {Name= "CSS", StartDate= "Septiembre 2020", EndDate= "Presente"},
+                          new Skill {Name= "JavaScript", StartDate= "Septiembre 2020", EndDate= "Presente"},
+                          new Skill {Name= "C#", StartDate= "Septiembre 2020", EndDate= "Octubre 2021"},
+                          new Skill {Name= ".Net Core", StartDate= "Septiembre 2020", EndDate= "Octubre 2021"},
+                          new Skill {Name= ".Net Framework", StartDate= "Septiembre 2020", EndDate= "Octubre 2021"},
+                          new Skill {Name= "Inyeccion de dependencias", StartDate= "Septiembre 2024", EndDate= "Presente"},
+                          new Skill {Name= "Swagger", StartDate= "Septiembre 2024", EndDate= "Presente"},
+                          new Skill {Name= "jQuery", StartDate= "Septiembre 2024", EndDate= "Presente" }
+                      } 
+                  },
+                  new SkillGroup
+                  {
+                      Title = "Bases de datos",
+                      Skills = new List<Skill>
+                      {
+                          new Skill {Name= "SQL Server", StartDate= "Septiembre 2020", EndDate= "Septiembre 2024"},
+                          new Skill {Name= "MySQL", StartDate= "Septiembre 2020", EndDate= "Septiembre 2024"},
+                          new Skill {Name= "Oracle", StartDate= "Septiembre 2020", EndDate= "Septiembre 2024"}
+                      }
+                  },
+                  new SkillGroup
+                  {
+                      Title = "Otras habilidades",
+                      Skills = new List<Skill>
+                      {
+                          new Skill {Name= "Git", StartDate= "Septiembre 2020", EndDate= "Octubre 2021"},
+                          new Skill {Name= "Bootstrap", StartDate= "Septiembre 2020", EndDate= "Octubre 2021"},
+                          new Skill {Name= "Docker", StartDate= "Septiembre 2020", EndDate= "Octubre 2021"},
+                          new Skill {Name= "SAP BusinessObjects", StartDate= "Octubre 2021", EndDate= "Septiembre 2024"},
+                          new Skill {Name= "Toad", StartDate= "Octubre 2021", EndDate= "Septiembre 2024"},
+                          new Skill {Name= "PowerCenter", StartDate= "Octubre 2021", EndDate= "Septiembre 2024"},
+                          new Skill {Name= "Programacion Orientada a Objetos", StartDate= "Septiembre 2020", EndDate= "Presente"},
+                          new Skill {Name= "SCRUM", StartDate= "Septiembre 2020", EndDate= "Septiembre 2024"}
+                      }
+                  }
+                },
+                Work = new List<Trabajo>
+                {
+                    new Trabajo {Titulo= "Ingeniero de datos", Institucion= "SuKarne", Fecha= "Octubre 2021 - Septiembre 2024"},
+                    new Trabajo {Titulo= "Desarrollador Junior", Institucion= "Heladísimo", Fecha= "Septiembre 2020 - Octubre 2021"},
+                },
+                Education = new List<Educacion>
+                {
+                    new Educacion {Titulo= "Ingeniería en Sistemas Computacionales", Institucion= "Instituto Tecnologico de Mérida", Fecha= "Agosto 2016 - Diciembre 2020"}
+                },
+                Project = new List<Proyecto>
+                {
+                    new Proyecto {Titulo= "CV Virtual", Imagen= "images/CVweb.png", Descripcion= "Es una pagina web construida desde FrontEnd donde se muestra en tiempo real todo lo relacionado a mis habilidades y formacion academica. Para hacer esta página se usaron las siguientes tecnologías: HTML, CSS, JavaScrip", Link= "https://github.com/VeroMV-24/CVVirtual"}
+                }
+            };
+            return Ok(curriculum);
+        }
+    }
+}
---
+++ CVWebAPI/Models/Curriculum.cs
@@ -0,0 +1,47 @@
+﻿namespace CVWebAPI.Models
+{
+    public class Curriculum
+    {
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public List<SkillGroup> SkillsData { get; set; }
+        public List<Trabajo> Work { get; set; }
+        public List<Educacion> Education { get; set; }
+        public List<Proyecto> Project { get; set; }
+    }
+
+    public class Skill
+    {
+        public string Name { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+
+    public class SkillGroup
+    {
+        public string Title { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+
+    public class Trabajo
+    {
+        public string Titulo { get; set; }
+        public string Institucion { get; set; }
+        public string Fecha { get; set; }
+    }
+
+    public class Educacion
+    {
+        public string Titulo { get; set; }
+        public string Institucion { get; set; }
+        public string Fecha { get; set; }
+    }
+
+    public class Proyecto
+    {
+        public string Titulo { get; set; }
+        public string Imagen { get; set; }
+        public string Descripcion { get; set; }
+        public string Link { get; set; }
+    }
+}
